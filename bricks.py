a7aabc744212316d47d8c15a1564f796d7ff804d
Okamiryuku <okamiryuku@gmail.com>
2023-11-04 12:55:13 -0600

    Brick wall creation

--- bricks.py
+++ bricks.py
@@ -3,22 +3,14 @@ from turtle import Turtle
 
 class Brick(Turtle):
 
-    def __init__(self, paddle_pos):
+    def __init__(self, brick_pos, width):
         super().__init__()
         self.speed("fastest")
         self.shape("square")
         self.color("white")
         self.penup()
-        self.turtlesize(stretch_wid=5, stretch_len=1)
-        self.goto(paddle_pos)
-
-    def up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
-
-    def down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
+        self.turtlesize(stretch_wid=2, stretch_len=width)
+        self.goto(brick_pos)
 
     def delete_brick(self):
         pass
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from paddle import Paddle
 from ball import Ball
 from score import ScoreBoard
 from bricks import Brick
-
+import random
 
 # Window Setup
 screen = Screen()
@@ -16,6 +16,26 @@ screen.tracer(0)
 # Calling classes
 paddle = Paddle((0, -550))
 ball = Ball((0, -525))
+list_bricks = []  # My list of Bricks Objects
+
+# Set the initial position for the first line of bricks
+y_pos = 300
+brick_count = 14  # Number of bricks in a row
+total_width = 815  # Total width for the row
+
+# Calculate the fixed distance between bricks
+brick_width = total_width / brick_count
+
+# Loop to create multiple rows of bricks
+for _ in range(4):
+    x_pos = -total_width / 2
+    for _ in range(brick_count):
+        # Generate a random width for the brick
+        random_width = random.uniform(0.5, 2.8)
+        brick = Brick((x_pos, y_pos), random_width)
+        list_bricks.append(brick)
+        x_pos += brick_width
+    y_pos -= 50
 
 # Player Scoreboard
 scoreboard = ScoreBoard(0)
@@ -42,6 +62,5 @@ while game_is_on:
 
     if ball.ycor() < -600:
         scoreboard.game_over()
-
 
 screen.exitonclick()
